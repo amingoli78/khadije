5e8fccc4f1c0f3d0764f1b154b18db1530905fa1
amingoli78 <amin.foli78@gmail.com>
2019-01-26 21:59:35 +0330

    update about add btn back add title in tollbar

--- app/src/main/java/com/ermile/khadije/about_us.java
+++ app/src/main/java/com/ermile/khadije/about_us.java
@@ -1,12 +1,17 @@
 package com.ermile.khadije;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toolbar;
+
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -18,41 +23,35 @@ import org.jsoup.Jsoup;
 
 public class about_us extends AppCompatActivity {
 
-    TextView titles, desc;
-    Button back;
+    LinearLayout layout;
+    TextView desc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_us);
 
-        titles = findViewById(R.id.about_title);
+        layout = findViewById(R.id.mother_about_us);
         desc  = findViewById(R.id.about_desc);
-        back = findViewById(R.id.btn_back_about);
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
 
         final SharedPreferences shared = getSharedPreferences("Prefs", MODE_PRIVATE);
-        final SharedPreferences.Editor editor = shared.edit();
-
         final Boolean farsi = shared.getBoolean("farsi", false);
         final Boolean arabic = shared.getBoolean("arabic", false);
         final Boolean english = shared.getBoolean("english", false);
 
-
-        if (farsi){
-            back.setText("بازگشت");
-        }
-        if (arabic){
-            back.setText("عودة");
+        if (farsi || arabic){
+            ViewCompat.setLayoutDirection(layout,ViewCompat.LAYOUT_DIRECTION_RTL);
         }
         if (english){
-            back.setText("back");
+            ViewCompat.setLayoutDirection(layout,ViewCompat.LAYOUT_DIRECTION_LTR);
         }
+
+        if(getSupportActionBar()!=null)
+        {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+
 
         String url = "";
 
@@ -80,8 +79,8 @@ public class about_us extends AppCompatActivity {
                 try {
 
                     String title = response.getString("title");
+                    getSupportActionBar().setTitle(title);
                     String dees = response.getString("content");
-                    titles.setText(title);
                     String desc_decod = Jsoup.parse(dees).body().text();
                     desc.setText(desc_decod);
 
@@ -104,6 +103,16 @@ public class about_us extends AppCompatActivity {
         AppContoroler.getInstance().addToRequestQueue(req);
         // END JSON
 
-
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if( id == android.R.id.home ){
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
