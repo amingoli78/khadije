589995d1893c8fe87092906110d9f389b842b043
amingoli78 <amin.goli78@gmail.com>
2019-09-17 18:51:18 +0430

    fix new

---
+++ app/src/main/java/com/ermile/khadije/Items/item_Main.java
@@ -0,0 +1,21 @@
+package com.ermile.khadije.Items;
+
+public class item_Main {
+
+    public static int LINK_1 = 1;
+    public static int LINK_2 = 2;
+    public static int LINK_Desc = 3;
+    public static int LINK_4 = 4;
+    public static int TITEL_link = 20;
+    public static int TITEL_NONE = 21;
+    public static int SLIDER = 30;
+    public static int SALAVAT = 40;
+    public static int HADITH = 50;
+    public static int NEWS  = 60;
+    public static int HR = 100;
+
+
+
+
+
+}
