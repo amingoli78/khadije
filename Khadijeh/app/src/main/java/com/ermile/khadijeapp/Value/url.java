dc1c3df2881896749b82b6c6bd3d1fdd4d4c2d88
amingoli78 <amin.goli78@gmail.com>
2019-11-23 17:29:09 +0330

    oder fix

---
+++ Khadijeh/app/src/main/java/com/ermile/khadijeapp/Value/url.java
@@ -0,0 +1,13 @@
+package com.ermile.khadijeapp.Static;
+
+public class url {
+
+    public static String app = "https://khadije.com/api/v6/app";
+    public static String salawat = "https://khadije.com/fa/api/v6/salawat";
+    public static String news = "https://khadije.com/fa/api/v6/posts/get?id=";
+    public static String del = "https://khadije.com/fa/api/v6/delneveshte";
+    public static String del_like = "https://khadije.com/fa/api/v6/delneveshte/like";
+    public static String token = "https://khadije.com/fa/api/v6/token";
+    public static String user_add = "https://khadije.com/fa/api/v6/android/user/add";
+    public static String language = "https://khadije.com/fa/api/v6/language";
+}
